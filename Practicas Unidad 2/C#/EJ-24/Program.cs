e3c1f08f6e9058bf322032e20b9d63efda3c0d89
Juan <juan.ahumada94@gmail.com>
2024-10-05 14:03:27 -0500

    EJ 24 Completado

---
+++ Practicas Unidad 2/C#/EJ-24/Program.cs
@@ -0,0 +1,27 @@
+﻿using EJ_24;
+
+Promedio miPromedio = new Promedio();
+
+miPromedio.C1 = 70;
+miPromedio.C2 = 80;
+miPromedio.C3 = 90;
+
+Console.WriteLine(miPromedio.Mensaje());
+
+miPromedio.C1 = 70;
+miPromedio.C2 = 71;
+miPromedio.C3 = 76;
+
+Console.WriteLine(miPromedio.Mensaje());
+
+miPromedio.C1 = 100;
+miPromedio.C2 = 100;
+miPromedio.C3 = 100;
+
+Console.WriteLine(miPromedio.Mensaje());
+
+miPromedio.C1 = 90;
+miPromedio.C2 = 98;
+miPromedio.C3 = 96;
+
+Console.WriteLine(miPromedio.Mensaje());
