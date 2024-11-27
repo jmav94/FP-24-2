42c96b2dabfdf551b4064a9433649b6ad79050cd
Juan Ahumada <juan.ahumada94@gmail.com>
2024-11-27 09:41:11 -0600

    Ejercicio 1 practica arreglos

---
+++ Practicas Unidad 4/C#/Ejercicio-1/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] miArreglo;
+        int celda, tamano;
+
+        Console.WriteLine("Captura el tamaño del arreglo:");
+        tamano = int.Parse(Console.ReadLine());
+
+        miArreglo = new int[tamano];
+
+        for (celda = 0; celda < tamano; celda++)
+        {
+            Console.WriteLine($"Capture el valor de la celda en la posicion {celda}");
+            miArreglo[celda] = int.Parse(Console.ReadLine());
+        }
+
+        quitarNegativos(ref miArreglo);
+
+        Console.WriteLine("Valores del arreglo sin negativos");
+
+        foreach (int numero in miArreglo)
+        {
+            Console.WriteLine(numero);
+        }
+
+    }
+    private static void quitarNegativos(ref int[] unArreglo){
+        for (int i = 0; i < unArreglo.Length; i++)
+        {
+            if (unArreglo[i] < 0)
+            {
+                unArreglo[i] = 0;
+            }
+        }
+    }
+}
